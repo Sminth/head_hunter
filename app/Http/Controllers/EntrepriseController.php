c52bb247249412f796b82f4ee4187c89c007c6e2
sminth <emmanuelmalan225@gmail.com>
2024-03-20 21:50:12 +0000

    maj migration

---
+++ app/Http/Controllers/EntrepriseController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\entreprise;
+use App\Http\Requests\StoreentrepriseRequest;
+use App\Http\Requests\UpdateentrepriseRequest;
+
+class EntrepriseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreentrepriseRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(entreprise $entreprise)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(entreprise $entreprise)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateentrepriseRequest $request, entreprise $entreprise)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(entreprise $entreprise)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SecteurController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\secteur;
+use App\Http\Requests\StoresecteurRequest;
+use App\Http\Requests\UpdatesecteurRequest;
+
+class SecteurController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoresecteurRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(secteur $secteur)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(secteur $secteur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatesecteurRequest $request, secteur $secteur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(secteur $secteur)
+    {
+        //
+    }
+}
